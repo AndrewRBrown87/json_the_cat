e472b29843e4974daec33542ab34555ddc189cea
Andrew Brown <andrew.robert.brown@gmail.com>
2022-11-18 20:03:24 +0000

    Add index.js breedFetcherTest.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,18 @@
 const request = require('request');
-let args = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const data = JSON.parse(body);
-    try {
-      console.log(data[0].description);
-    } catch (err) {
-      console.log("Breed not found");
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      const data = JSON.parse(body);
+      try {
+        callback(null, data[0].description);
+      } catch (err) {
+        callback("Breed not found", null);
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
